51bff069796df70be0a3b86db057f1f0efa2eea8
Hedde <me@heddevanderheide.nl>
2014-08-20 11:53:42 +0200

    updated _networth method

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='django-networth',
-    version='0.2',
+    version='0.3',
     packages=['networth'],
     url='https://github.com/Hedde/django-networth',
     license='MIT',
     author='Hedde van der Heide',
     author_email='me@heddevanderheide.nl',
-    description='valued instances'
+    description='Valuate instances of Django Models.'
 )
