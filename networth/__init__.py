4477c3e93453037159c42ac58039b908ea3909e0
Hedde <me@heddevanderheide.nl>
2014-08-20 08:55:22 +0200

    updated files

--- networth/__init__.py
+++ networth/__init__.py
@@ -1,10 +1,1 @@
 __author__ = 'heddevanderheide'
-
-
-"""
-note
-
-It would be nice if networth was implemented in
-a way the Meta class on Django's Models is
-implemented
-"""
--- networth/models.py
+++ networth/models.py
@@ -6,45 +6,6 @@ from django.db.models import Model
 
 
 class NetworthModel(Model):
-    """
-    Implementation example
-
-    class Object(NetworthModel):
-        first_name = models.CharField(max_length=25)
-        last_name = models.CharField(max_length=75, blank=True, null=True)
-
-        tags = TaggableManager(through=TaggedItem, blank=True)
-
-        class Networth:
-            fields = (
-                ('first_name', (True, 1)),
-                ('last_name', (lambda f: f.startswith('P'), 5)),
-                ('tags', (lambda f: f.count(), 'result'))
-            )
-
-        Consider the following pseudo instances:
-
-        ('Pete',).networth() > 1
-        ('Pete', 'James').networth() > 1
-        ('Pete', 'Philly').networth() > 6
-
-        A more complex example:
-
-        ('Pete', 'Philly', <TagManager>).networth() > 0
-
-        In this example 'result' uses the outcome of the function as the net result
-        As we have not added any tags to Pete yet, the count() will be zero.
-
-
-        Declaring your own Networh logic:
-
-        from networth.models import NetworthModel as BaseNetworthModel
-
-        class NetworthModel(BaseNetworthModel):
-            def networth(self):
-                return super(NetworthModel, self).networth()
-
-    """
     networth = models.IntegerField()
 
     class Meta:
