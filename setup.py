816d68a573700c44ff429b119f3027f61a13b353
Hedde <me@heddevanderheide.nl>
2014-08-30 13:38:20 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-networth',
-    version='0.6',
+    version='0.7',
     packages=['networth'],
     url='https://github.com/Hedde/django-networth',
     license='MIT',
