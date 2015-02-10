68568bb8f4b6155d1affd842478fcc7c442febd3
Hedde <me@heddevanderheide.nl>
2015-02-10 11:54:35 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-networth',
-    version='1.1',
+    version='1.2',
     packages=['networth'],
     url='https://github.com/Hedde/django-networth',
     license='MIT',
